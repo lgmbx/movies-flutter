df0c3b56dc015e41950f2560b6fa609156a7e1a7
Luiz Gustavo <gustavo_x@live.com>
2020-11-29 12:45:57 -0300

    desafio 1

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,20 @@ import 'package:flutter/material.dart';
 import 'core/constants.dart';
 import 'core/theme_app.dart';
 import 'pages/movie_page.dart';
+import 'package:custom_splash/custom_splash.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: CustomSplash(
+      imagePath: 'assets/movie-splash.png',
+      backGroundColor: Colors.white10,
+      animationEffect: 'zoom-in',
+      logoSize: 200,
+      home: MyApp(),
+      duration: 2500,
+      type: CustomSplashType.StaticDuration,
+    ),
+  ));
 }
 
 class MyApp extends StatelessWidget {
